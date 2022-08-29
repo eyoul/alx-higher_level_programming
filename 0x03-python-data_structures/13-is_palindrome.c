cde8e306b45a0dc3db127ae3c896b811e8350a55
eyoul <eyoulshimeles2@gmail.com>
2022-08-29 12:51:09 -0700

    repo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,40 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
-  * is_palindrome - checks if a singly linked list is a palindrome
-  * @head: double pointer to list.
-  *
-  *Return: 1 if palindrome, 0 if not palindrom.
-  */
+ * is_palindrome - check if linked list is a palindrome
+ * @head: pointer to pointer to head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-	return (checkPalindrome(head, *head));
-}
+	listint_t *temp = *head;
+	int size = sizeof(int);
+	int i = 1;
+	int left = 0;
+	int right = i - 1;
+	int *arr = malloc(100000 * sizeof(int));
 
-/**
-  * checkPalindrome - recursive function ot check if sinly linked list
-  * is a palindrome.
-  * @headptr: double pointer to list.
-  * @tptr: pointer to list.
-  *
-  * Return: 1 or 0
-  */
-int checkPalindrome(listint_t **headptr, listint_t *tptr)
-{
-	int res;
+	arr[0] = (*head)->n;
 
-	/* base case */
-	if (tptr == NULL)
+	if (*head == NULL)
 		return (1);
-	res = checkPalindrome(headptr, tptr->next) && ((*headptr)->n == tptr->n);
-	return (res);
+
+	temp = temp->next;
+
+	while (temp != NULL)
+	{
+		size += sizeof(int);
+		arr = realloc(arr, size);
+		arr[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+	for (; left < right; left++, right--)
+	{
+		if (arr[left] != arr[right])
+			return (0);
+	}
+	return (1);
 }
