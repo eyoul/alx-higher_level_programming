1546415e6fe7c5c76c94abaf2aa203bf302cd8b4
eyoul <eyoulshimeles2@gmail.com>
2022-08-29 12:43:37 -0700

    PyRepo

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,5 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[i])):
-                if j != len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
-    else:
-        print()
+    for i in matrix:
+        print(" ".join("{:d}".format(j) for j in i))
