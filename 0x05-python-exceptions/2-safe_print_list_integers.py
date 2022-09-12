aa6ecd1f891c4f8fe8ea1ab951c69390176769d9
eyoul <eyoulshimeles2@gmail.com>
2022-09-12 13:24:04 -0700

    PyRepo

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    for j in range(x)
+    for j in range(x):
         try:
             print("{:d}".format(my_list=[j]), end="")
             i += 1
