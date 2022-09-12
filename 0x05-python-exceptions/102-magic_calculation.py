1c1c6609ee1c9c9e5880144adc46a265d43db441
eyoul <eyoulshimeles2@gmail.com>
2022-09-12 14:23:53 -0700

    repo

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,5 +3,15 @@
 
 
 def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
     """A function based on a python bytee code"""
     return (result)
