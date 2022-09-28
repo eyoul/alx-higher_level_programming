8144cda7cd0ee75f94f0c778071a9862b7abdba3
eyoul <eyoulshimeles2@gmail.com>
2022-09-28 13:54:33 -0700

    PyRepo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ script that adds all arguments to a Python list function
 """
 
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
