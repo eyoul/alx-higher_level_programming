b01bb44c0c52e4f70a164ea5c64196548f91a9f5
eyoul <eyoulshimeles2@gmail.com>
2022-09-28 11:43:17 -0700

    PyRepo

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-Contains the number_of_lines function
+function that writes a string to a text file 
 """
 
 def write_file(filename="", text=""):
-     """returns the number of lines of a text file"""
-     with open(filename, 'r', encoding='utf-8')as f:
-         i = 0
-         for line in f
-            i += 1
-        return i
+    """ returns the number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
