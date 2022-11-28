8c018338efe45dae11bb292455cee7c7f271d1ab
eyoul <49184150+eyoul@users.noreply.github.com>
2022-11-29 00:41:39 +0300

    branch

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
-=======
-const myVar ='JavaScript is amazing';
-console.log(myVar);
->>>>>>> 85acfb74584ba270785402a9107c58450ae2db33
