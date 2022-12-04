caf0f49512c42e8c8e22e31742ee5925b53f853e
eyoul <eyoulshimeles2@hmail.com>
2022-12-04 13:27:30 -0800

    Object Relational Map fix

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Deletes all State objects with a name containing
 the letter a from the database hbtn_0e_6_usa.
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,11 +2,10 @@
 """
 Displays all values in the states table of the database hbtn_0e_0_usa
 whose name matches that supplied as argument.
-Safe from SQL injections.
-Usage: ./3-my_safe_filter_states.py <mysql username> \
-                                     <mysql password> \
-                                     <database name> \
-                                     <state name searched>
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
 import sys
 import MySQLdb
@@ -14,5 +13,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
